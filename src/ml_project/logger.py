48db87629a60da47b57d12a2044e2b95231fa21b
kshitij456258 <74782289+kshitij456258@users.noreply.github.com>
2023-09-11 01:41:36 +0530

    Add files via upload

---
+++ src/ml_project/logger.py
@@ -0,0 +1,17 @@
+import logging 
+import os
+
+from datetime import datetime
+
+
+LOG_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs", LOG_file)
+os.makedirs(log_path, exist_ok=True)
+
+LOG_file_path=os.path.join(log_path, LOG_file)
+
+logging.basicConfig(
+    filename=LOG_file_path,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
